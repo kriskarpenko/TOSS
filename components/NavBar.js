189585b13be77a1cae3a03273f9d9d3a9797a656
Kristina Karpenko <karpenko.krista@gmail.com>
2022-11-08 17:16:24 +0100

    feat: footer + navbar

---
+++ components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+const NavBar = () => {
+  return (
+    <div className={styles.navBar}>
+      <div className={styles.title}> TAMARA'S OPERA SINGERS SCHOOL</div>
+    </div>
+  );
+};
+
+export default NavBar;
