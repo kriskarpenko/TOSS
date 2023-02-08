08d07b9cd44c6333779f7a4eb70a3e1311dd023e
Kristina Karpenko <karpenko.krista@gmail.com>
2023-02-08 21:36:31 +0100

    update events

--- pages/events.js
+++ pages/events.js
@@ -7,7 +7,23 @@ const EventPage = () => {
     <div>
       <Section>
         <h1>Events</h1>
+        <h2>Upcoming event</h2>
       </Section>
+      <Section pictureSrc="/img/logoDiCita.jpeg" customClass="introduction">
+        <h2>Spring Concert</h2>
+        <h3>Mozart & Cervo Liguria </h3>
+        <h3>Opéra Le nozze di Figaro </h3>
+        <p>
+          Young talents and soloists of Tamara's Opera Singers School are taking part in the program “Le nozze di
+          Figaro”:
+        </p>
+        <ul className="offersUl">
+          <li>Vocal Couch - Tamara Tarskikh</li>
+          <li>Maestro - Francesco Barbagelata</li>
+          <li>Stage director - Elena Beck</li>
+        </ul>
+      </Section>
+      <Section pictureSrc="/img/photos/cervoTown.jpeg" />
       <div>
         {/* <iframe
           src="https://player.vimeo.com/video/791852977?h=6ae4e88e53&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
